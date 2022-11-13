394a64996161292794d1ba1fbde56de50b0c6ff1
BuildTools <unconfigured@null.spigotmc.org>
2022-11-13 18:47:46 +0100

    - added feedback de/activating persist mode - added feedback when item is added to the sign - command pipeadd_persist -> pipeadd persist - left click adds item to the 4. line, while right click adds item to the 3. line

--- src/main/java/com/github/Emcc13/CraftbookPipeCreator.java
+++ src/main/java/com/github/Emcc13/CraftbookPipeCreator.java
@@ -1,5 +1,6 @@
 package com.github.Emcc13;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Sign;
 import org.bukkit.block.data.type.WallSign;
@@ -12,7 +13,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
@@ -32,10 +32,10 @@ public class CraftbookPipeCreator extends JavaPlugin implements Listener, Runnab
         persistendPlayer = new HashSet<UUID>();
 
         PipeAdd pipeAdd = new PipeAdd(this);
-        PipeAddPersist pipeAddPersist = new PipeAddPersist(this);
+//        PipeAddPersist pipeAddPersist = new PipeAddPersist(this);
 
         getCommand("pipeadd").setExecutor(pipeAdd);
-        getCommand("pipeadd_persist").setExecutor(pipeAddPersist);
+//        getCommand("pipeadd_persist").setExecutor(pipeAddPersist);
         getServer().getPluginManager().registerEvents(this, this);
         Bukkit.getScheduler().runTaskTimerAsynchronously(
                 this, this, 5 * 20,
@@ -56,35 +56,47 @@ public class CraftbookPipeCreator extends JavaPlugin implements Listener, Runnab
         @Override
         public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
             if (sender instanceof Player && (((Player) sender).hasPermission("pipeCreator") || sender.isOp())) {
-                this.main.activePlayer.put(((Player) sender).getUniqueId(), new Timestamp(System.currentTimeMillis()));
+                if (args.length > 0) {
+                    if (args[0].toLowerCase().equals("persist")) {
+                        this.main.toggle_persist(((Player) sender));
+                    } else {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&bPXFL&7-&bPipe&7] verwende '/pipeadd' oder '/pipeadd persist'"));
+                    }
+                } else {
+                    this.main.activePlayer.put(((Player) sender).getUniqueId(), new Timestamp(System.currentTimeMillis()));
+                }
             }
             return false;
         }
     }
 
-    private void toggle_persist(UUID player){
-        if (this.persistendPlayer.contains(player)){
-            this.persistendPlayer.remove(player);
-        }else {
-            this.persistendPlayer.add(player);
+    private void toggle_persist(Player player) {
+        UUID player_id = player.getUniqueId();
+        if (this.persistendPlayer.contains(player_id)) {
+            this.persistendPlayer.remove(player_id);
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&bPXFL&7-&bPipe&7] &3Pipe-Persist ist nun &cdeaktiviert&3!"));
+        } else {
+            this.persistendPlayer.add(player_id);
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7[&bPXFL&7-&bPipe&7] &3Pipe-Persist ist nun &aaktiviert&3!"));
         }
+        this.activePlayer.remove(player_id);
     }
 
-    private class PipeAddPersist implements CommandExecutor {
-        private CraftbookPipeCreator main;
-
-        public PipeAddPersist(CraftbookPipeCreator creator) {
-            main = creator;
-        }
-
-        @Override
-        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-            if (sender instanceof Player && (((Player) sender).hasPermission("pipeCreator") || sender.isOp())) {
-                this.main.toggle_persist(((Player) sender).getUniqueId());
-            }
-            return false;
-        }
-    }
+//    private class PipeAddPersist implements CommandExecutor {
+//        private CraftbookPipeCreator main;
+//
+//        public PipeAddPersist(CraftbookPipeCreator creator) {
+//            main = creator;
+//        }
+//
+//        @Override
+//        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+//            if (sender instanceof Player && (((Player) sender).hasPermission("pipeCreator") || sender.isOp())) {
+//                this.main.toggle_persist(((Player) sender));
+//            }
+//            return false;
+//        }
+//    }
 
     @Override
     public void run() {
@@ -97,28 +109,32 @@ public class CraftbookPipeCreator extends JavaPlugin implements Listener, Runnab
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event){
+    public void onPlayerQuit(PlayerQuitEvent event) {
         UUID player = event.getPlayer().getUniqueId();
-        if (this.persistendPlayer.contains(player)){
+        if (this.persistendPlayer.contains(player)) {
             this.persistendPlayer.remove(player);
         }
     }
 
     @EventHandler
-    public void onPlayerChangeWord(PlayerChangedWorldEvent event){
+    public void onPlayerChangeWord(PlayerChangedWorldEvent event) {
         UUID player = event.getPlayer().getUniqueId();
-        if (this.persistendPlayer.contains(player)){
+        if (this.persistendPlayer.contains(player)) {
             this.persistendPlayer.remove(player);
         }
     }
-
 
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         try {
-            if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            int line_idx;
+            if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                line_idx = 2;
+            } else if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
+                line_idx = 3;
+            } else {
                 return;
             }
             if (!(event.getClickedBlock().getBlockData() instanceof WallSign ||
@@ -134,9 +150,12 @@ public class CraftbookPipeCreator extends JavaPlugin implements Listener, Runnab
             }
             String toAdd = getStringFromItem(player.getInventory().getItemInMainHand());
             Sign s = (Sign) event.getClickedBlock().getState();
-            s.setLine(2, s.getLine(2) + ((s.getLine(2).length() > 1) ? "," : "") + toAdd);
+            s.setLine(line_idx, s.getLine(line_idx) + ((s.getLine(line_idx).length() > 1) ? "," : "") + toAdd);
             s.update();
             this.activePlayer.remove(event.getPlayer().getUniqueId());
+            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes(
+                    '&', "&7[&bPXFL&7-&bPipe&7] &3%ITEM% wurde dem Pipe Schild in der %LINE%. Zeile hinzugefügt!"
+                            .replace("%ITEM%", toAdd).replace("%LINE%", String.valueOf(line_idx + 1))));
         } catch (NullPointerException e) {
         }
     }
